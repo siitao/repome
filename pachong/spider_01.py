7c07d99ff0d5f214512116bbef810a9d750bda6a
siitao <siitao@users.noreply.github.com>
2015-12-18 10:55:29 +0800

    change

--- pachong/spider_01.py
+++ pachong/spider_01.py
@@ -5,7 +5,7 @@ import re
 import urllib2
 url_head = "http://tieba.baidu.com" 
 # 定义过滤规则
-reg = '<a href="(.*)" title="(.*)" target="_blank" class="j_th_tit ">(.*)</a></div>'
+reg = '<a href="(.*)" title="(.*)" target'
 try:
     tieba = raw_input("please put the tieba name:")
     times = int(raw_input("please input the page number:"))
@@ -14,7 +14,7 @@ try:
             url = "http://tieba.baidu.com/f?kw=%s&ie=utf-8&pn=%d" %(tieba,i)
             data = urllib2.urlopen(url).read()
             result = re.findall(reg,data)  # 在结果中找出符合规则的内容
-            for j,k,l in result:
+            for j,k in result:
 		print url_head+j,k
 	        f.write(url_head+j+' '+k+'\n')      # 把想要的数据存放在一个文件当中
 except:
